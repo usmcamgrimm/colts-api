98d65e665c513e9ee5a852489b4598a1d1800d00
Adrian Grimm <usmcamgrimm@gmail.com>
2025-06-07 11:38:51 -0400

    Update the schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_06_01_181600) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_07_020531) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -76,6 +76,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_06_01_181600) do
     t.datetime "updated_at", null: false
     t.integer "year"
     t.integer "def_td"
+    t.integer "fumble_rec"
     t.index ["player_id"], name: "index_season_stats_on_player_id"
   end
 
