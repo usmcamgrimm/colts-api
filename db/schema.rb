c4b9050379a22e457e0d800af839b5b39d7fa356
Adrian Grimm <usmcamgrimm@gmail.com>
2026-01-10 10:09:11 -0500

    Run migration to change player numbers to strings

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_12_30_135629) do
+ActiveRecord::Schema[8.1].define(version: 2026_01_10_150551) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -18,7 +18,7 @@ ActiveRecord::Schema[8.1].define(version: 2025_12_30_135629) do
     t.string "college"
     t.datetime "created_at", null: false
     t.string "name"
-    t.integer "number"
+    t.string "number"
     t.text "photo"
     t.datetime "updated_at", null: false
   end
