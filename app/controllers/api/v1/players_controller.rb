8db5d4b8911dcebf5b89f6ab20fc0ac7989be354
Adrian Grimm <usmcamgrimm@gmail.com>
2026-06-15 08:45:54 -0400

    Force json formatting

--- app/controllers/api/v1/players_controller.rb
+++ app/controllers/api/v1/players_controller.rb
@@ -1,11 +1,11 @@
 class Api::V1::PlayersController < ApplicationController
   def index
     @players = Player.all
-    render :index, formats: :json
+    render :index, formats: [ :json ]
   end
 
   def show
     @player = Player.find(params[:id])
-    render :index, formats: :json
+    render :index, formats: [ :json ]
   end
 end
